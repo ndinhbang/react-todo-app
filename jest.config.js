c82ac342cbb03f65d977595eccb9e3bc85e02489
Nguyen Dinh Bang <bangnd@abilive.vn>
2023-05-29 10:55:31 +0700

    set up jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testRegex: 'src/__tests__/.*.test.js$',
+  //   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: [
+    'node_modules',
+    __dirname, // the root directory
+  ],
+  // ... other options ...
+};
